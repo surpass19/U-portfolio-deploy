bf6167fbf23c8f152a6410b2d2d4603fb43cec35
surpass19 <surpass0219r@gmail.com>
2021-09-16 01:14:56 +0900

    SECRET_KEY変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,8 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fx1d2@s^#jbm75^szid2yzzt1tll4@08awuep12u_ae_ox9#82'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
