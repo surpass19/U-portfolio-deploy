fdc07bfcb5e1e80d6cb701977867d9b4cb833bc9
surpass19 <surpass0219r@gmail.com>
2021-09-16 01:41:12 +0900

    SECRET_KEY変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,11 +27,13 @@ try:
 except ImportError:
     pass
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
